6d0ca78552a1a96d0995793f8d7b6d9ebdb1120b
htyang <htyang@Ctrip.com>
2016-11-06 10:16:24 +0800

    新建消息

---
+++ UI/Business/NewMsg/TLNewMsgVC.swift
@@ -0,0 +1,35 @@
+//
+//  TLNewMsgVC.swift
+//  todolist
+//
+//  Created by yht on 06/11/2016.
+//  Copyright © 2016 yht. All rights reserved.
+//
+
+import UIKit
+
+class TLNewMsgVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
